430e8b45b19a7fdc9a54a91e7dc57169b9658165
jonsnw21 <neeraj14.nj@gmail.com>
2018-08-29 03:08:55 +0530

    remove data folder from git ignore

---
+++ data/city.js
@@ -0,0 +1,166 @@
+let north = {
+    "name": "North",
+    "cities": [
+        {"name": "Delhi",
+            "townClass": "A"},
+        {"name": "Noida",
+            "townClass": "A"},
+        {"name": "Gurgaon",
+            "townClass": "A"},
+        {"name": "Jaipur",
+            "townClass": "B"},
+        {"name": "Chandigarh",
+            "townClass": "C"},
+        {"name": "Lucknow",
+            "townClass": "C"},
+        {"name": "Dehradun",
+            "townClass": "C"},
+        {"name": "Faridabad",
+            "townClass": "C"},
+        {"name": "Kanpur",
+            "townClass": "C"},
+        {"name": "Kota",
+            "townClass": "C"},
+        {"name": "Ludhiana",
+            "townClass": "C"},
+        {"name": "Varanasi",
+            "townClass": "C"},
+        {"name": "Amritsar",
+            "townClass": "C"},
+        {"name": "Ambala",
+            "townClass": "C"},
+        {"name": "Patiala",
+            "townClass": "C"},
+        {"name": "Allahabad",
+            "townClass": "C"},
+        {"name": "Jodhpur",
+            "townClass": "C"},
+        {"name": "Jalandhar",
+            "townClass": "C"},
+        {"name": "Agra",
+            "townClass": "C"},
+        {"name": "Karnal",
+            "townClass": "D"},
+        {"name": "Hissar",
+            "townClass": "D"},
+        {"name": "Meerut",
+            "townClass": "D"},
+        {"name": "Udaipur",
+            "townClass": "D"}
+
+    ]
+
+
+};
+
+let south = {
+    "name": "South",
+    "cities": [
+        {"name": "Chennai",
+            "townClass": "A"},
+        {"name": "Bangalore",
+            "townClass": "B"},
+        {"name": "Hyderabad",
+            "townClass": "B"},
+        {"name": "Scundrabad",
+            "townClass": "B"},
+        {"name": "Chennai",
+            "townClass": "B"},
+        {"name": "Tambaram",
+            "townClass": "C"},
+        {"name": "Visakhapatnam",
+            "townClass": "C"},
+        {"name": "Chengalpattu",
+            "townClass": "D"},
+        {"name": "Coimbatore",
+            "townClass": "D"},
+        {"name": "Udupi",
+            "townClass": "D"},
+        {"name": "Ernakulam",
+            "townClass": "D"},
+        {"name": "Trivandrum",
+            "townClass": "D"},
+        {"name": "Mysuru",
+            "townClass": "D"},
+        {"name": "Mangaluru",
+            "townClass": "D"},
+        {"name": "Vellore",
+            "townClass": "D"}
+
+    ]
+
+};
+
+let east = {
+    "name": "East",
+    "cities": [
+        {"name": "Kolkatta",
+            "townClass": "A"},
+        {"name": "Bhubaneswar",
+            "townClass": "B"},
+        {"name": "Patna",
+            "townClass": "C"},
+        {"name": "Guwahati",
+            "townClass": "C"},
+        {"name": "Ranchi",
+            "townClass": "C"},
+        {"name": "Cuttack",
+            "townClass": "D"},
+        {"name": "Howrah",
+            "townClass": "D"},
+        {"name": "Singhbhum",
+            "townClass": "D"},
+        {"name": "Manipur",
+            "townClass": "D"},
+        {"name": "Meghalaya",
+            "townClass": "D"},
+        {"name": "Nagaland",
+            "townClass": "D"}
+
+    ]
+
+
+};
+
+let west = {
+    "name": "West",
+    "cities": [
+        {"name": "Mumbai",
+            "townClass": "A"},
+        {"name": "Thane",
+            "townClass": "A"},
+        {"name": "New Mumbai",
+            "townClass": "A"},
+        {"name": "PUNE",
+            "townClass": "B"},
+        {"name": "AHMEDABAD",
+            "townClass": "C"},
+        {"name": "NAGPUR",
+            "townClass": "C"},
+        {"name": "INDORE",
+            "townClass": "C"},
+        {"name": "Goa",
+            "townClass": "C"},
+        {"name": "VADODARA",
+            "townClass": "C"},
+        {"name": "Surat",
+            "townClass": "C"},
+        {"name": "Bhopal",
+            "townClass": "C"},
+        {"name": "Palghar",
+            "townClass": "C"},
+        {"name": "Raipur",
+            "townClass": "C"},
+        {"name": "Nasik",
+            "townClass": "C"},
+        {"name": "Gwalior",
+            "townClass": "D"},
+        {"name": "Rajkot",
+            "townClass": "D"}
+
+    ]
+
+
+};
+
+module.exports = {north, south, east, west};
